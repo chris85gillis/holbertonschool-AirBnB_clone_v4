329cd1ef4da8cfa4cb78efd9bf595350d66017e5
colbertamir <colbertamir1516@gmail.com>
2023-12-11 04:14:22 +0000

    2 redone

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    let checkedAmenities = {};
+  
+    $('.amenities input[type="checkbox"]').change(function() {
+      const amenityId = $(this).data('id');
+      const amenityName = $(this).data('name');
+  
+      if ($(this).is(':checked')) {
+        checkedAmenities[amenityId] = amenityName;
+      } else {
+        delete checkedAmenities[amenityId];
+      }
+  
+      const amenitiesList = Object.values(checkedAmenities).join(', ');
+      $('.amenities h4').text(amenitiesList);
+    });
+  });
+  
